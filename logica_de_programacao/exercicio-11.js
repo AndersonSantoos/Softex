28a3b5b45399be189072ce424c8cfb3fbf7e2e87
Anderson Santos <110641610+DevAndersonSantos@users.noreply.github.com>
2023-07-22 17:46:29 -0300

    Create exercicio-11.js

---
+++ logica_de_programacao/exercicio-11.js
@@ -0,0 +1,14 @@
+let num1 = Number(prompt("Informe o primeiro número: "));
+let num2 = Number(prompt("Informe o segundo número: "));
+let num3 = Number(prompt("Informe o terceiro número: "));
+
+if(num1 > num2 && num1 > num3 ) {
+    console.log("O maior número é o 1! ");
+} else {
+
+} if(num2 > num1 && num2 > num3 ) {
+    console.log("O maior número é o 2! ")
+} else {
+    } if(num3 > num1 && num3 > num1 ) {
+    console.log("O maior número é o 3! ")
+}
