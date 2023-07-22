ab6aaf83aaa234f7914a8e2d37f76cd75e474503
Anderson Santos <110641610+DevAndersonSantos@users.noreply.github.com>
2023-07-22 17:46:48 -0300

    Create exercicio-12.js

---
+++ logica_de_programacao/exercicio-12.js
@@ -0,0 +1,9 @@
+let num = Number(prompt("Informe um número: "));
+
+if(num % 2 === 0) {
+    console.log("O número é par! ");
+} else {
+    if(num % 3 === 0) {
+        console.log("O número é ímpar! ")
+    }
+}
