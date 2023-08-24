5cae972c1ef07054dfde1ce9f0e67da2028c15eb
Anderson Santos <110641610+DevAndersonSantos@users.noreply.github.com>
2023-08-23 21:34:46 -0300

    Add files via upload
    Calculadora

---
+++ JavaScript/Semana2/calculadora.js
@@ -0,0 +1,35 @@
+// CALCULADORA
+
+function calculadora(valor1, valor2, operacao) {
+    let resultado
+     switch(operacao) {
+        case "+":
+            resultado = valor1 + valor2;
+                break;
+        case "-":
+            resultado = valor1 - valor2;
+                break;
+        case "*":
+            resultado = valor1 * valor2;
+                break;
+        case "/":
+            if(valor2 !== 0) {
+               resultado = (valor1 / valor2); // Resultado da divisão inteira
+               resto = valor1 % valor2; // Sobrando da divisão
+               return `Resultado: ${resultado}, Resto: ${resto}`;
+            } else {
+                return `Divisão por zero não é permitida.`
+            }
+            
+        default:
+            return "Operador inválido";                       
+  }
+            return `Resultado: ${resultado}`;
+}
+
+const valor1 = parseFloat(prompt(`Digite o primeiro valor: `));
+const valor2 = parseFloat(prompt(`Digite o segundo valor: `));
+const operador = prompt(`Digite o operador (+, -, * /)`)
+
+const resultado = calculadora(valor1, valor2, operador);
+console.log(`O resultado da operação é: ${resultado}`);
