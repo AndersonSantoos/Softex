35c228e5031779edfce1e4a623c3d7fa2d8b5794
Anderson Santos <110641610+DevAndersonSantos@users.noreply.github.com>
2023-09-17 15:11:09 -0300

    Manipulação de Strings.
    Manipulação de Strings.

---
+++ JavaScript/Semana5/exe_03/string.js
@@ -0,0 +1,18 @@
+// O concat() serve para concatenar duas ou mais strings em uma nova string. Ele não modifica as strings originais, mas cria uma nova string
+// contendo a concatenação das strings originais.
+
+let primeiroNome = "Anderson";
+let segundoNome = " Santos";
+let resultado = primeiroNome.concat(segundoNome);
+console.log(resultado);
+
+//==================================================================================
+
+//Método toUpperCase é utilizado para retornar todas letras maiúsculos.
+
+let texto = "Este é um exemplo de texto em minúsculas.";
+
+// Usando toUpperCase para transformar a string em maiúsculas
+let textoMaiusculo = texto.toUpperCase();
+
+console.log(textoMaiusculo); // Saída: "ESTE É UM EXEMPLO DE TEXTO EM MINÚSCULAS."
